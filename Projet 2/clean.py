c3d9d512f876ce1ac1c877fdd6982864befaf841
StanislasROLLAND <stanislasrolland05@gmail.com>
2025-11-04 11:53:04 +0100

    Cleaner Python

--- Projet 2/clean.py
+++ Projet 2/clean.py
@@ -4,11 +4,26 @@ def clean_data(file_path):
     # Load
     df = pd.read_csv(file_path)
 
-    # Supprime les doublons
+    # Suppression des doublons
     df.drop_duplicates(inplace=True)
 
-    # Supprimme les lignes avec une valeur manquante
+    # Suppression des lignes avec une valeur manquante
     df.dropna(inplace=True)
+
+    # Suppression de la colone 'Timestamp'
+    if ('Timestamp' in df.columns):
+        df.drop(columns = ['Timestamp'], inplace = True)
+
+    # Suppression de la colonne 'Primary streaming service'
+    if ('Primary streaming service' in df.columns):
+        df.drop(columns = ['Primary streaming service'], inplace = True)
+
+    # Suppression de la colonne 'Permissions'
+    if('Permissions' in df.columns):
+        df.drop(columns = ['Permissions'], inplace = True)
+
+    # Ajout de la colonne 'Id'
+    df.insert(0, 'Id', range(1, len(df) + 1))
 
     # Save
     df.to_csv('cleaned_dataset.csv', index=False)
