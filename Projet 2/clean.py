7438e9dcaedc542f0f58aa2a9b5bf545702a9ee2
StanislasROLLAND <stanislasrolland05@gmail.com>
2025-11-12 16:58:37 +0100

    Le BBL du raton laveur (Pierre a dit smash (je cite))

--- Projet 2/clean.py
+++ Projet 2/clean.py
@@ -1,31 +1,30 @@
 import pandas as pd
 
+file_path = 'dataset.csv'
+
 def clean_data(file_path):
-    # Load
+
+    # Chargement du fichier CSV
     df = pd.read_csv(file_path)
 
-    # Suppression des doublons
+    # Suppression des doublons et des lignes avec des valeurs manquantes
     df.drop_duplicates(inplace=True)
-
-    # Suppression des lignes avec une valeur manquante
     df.dropna(inplace=True)
 
-    # Suppression de la colone 'Timestamp'
+    # Suppression des colones 'Timestamp', 'Primary streaming service' et 'Permissions'
     if ('Timestamp' in df.columns):
         df.drop(columns = ['Timestamp'], inplace = True)
 
-    # Suppression de la colonne 'Primary streaming service'
     if ('Primary streaming service' in df.columns):
         df.drop(columns = ['Primary streaming service'], inplace = True)
 
-    # Suppression de la colonne 'Permissions'
     if('Permissions' in df.columns):
         df.drop(columns = ['Permissions'], inplace = True)
 
     # Ajout de la colonne 'Id'
     df.insert(0, 'Id', range(1, len(df) + 1))
 
-    # Save
+    # Enregistrement du DataFrame nettoyé dans un nouveau fichier CSV
     df.to_csv('cleaned_dataset.csv', index=False)
 
-clean_data('mxmh_survey_results.csv')
+clean_data(file_path)
